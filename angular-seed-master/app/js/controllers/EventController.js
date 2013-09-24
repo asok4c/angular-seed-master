5443a30db60d974facc63a9886978d077fefced3
Asok <ashokreddy898@gmail.com>
2013-09-23 23:56:43 -0500

    $http and $q service usage with promise
    $http and $q service usage with promise

--- angular-seed-master/app/js/controllers/EventController.js
+++ angular-seed-master/app/js/controllers/EventController.js
@@ -5,9 +5,20 @@ eventsApp.controller('EventController',
          $scope.sortorder='name';
        /*we can make it name, level as we are using this at session level we can use
         anything at that scope we can make upVoteCount*/
-         eventData.getEvent(function(event){
-             $scope.event =  event ;
-        }) ;
+
+
+             //  Interesting fact is that the event is binded to a promise returned from eventData
+             // which binds in view is ok, but if we try to access $scope.event.name , we cannot since
+             // it is a promise a possible recommendation is to send the model from view if it is
+             // available in view.
+             $scope.event =  eventData.getEvent() ;
+             console.log($scope.event);   // see in console this is a promise
+
+            $scope.event.then(function(event){
+                           console.log(event);    // in console this would be a plain object.
+            }, function(status)  {
+                         console.log(status);
+            } );
 
         $scope.upVoteSession = function(session){
                session.upVoteCount++ ;
--- angular-seed-master/app/js/services/EventData.js
+++ angular-seed-master/app/js/services/EventData.js
@@ -6,18 +6,21 @@
  * To change this template use File | Settings | File Templates.
  */
 
-eventsApp.factory('eventData',function($http, $log){
+eventsApp.factory('eventData',function($http, $log,$q){
       return {
-
-          getEvent: function(successcb){
-
-                   $http({method:'GET', url:'data/event/1.json'}).
+          getEvent: function(){
+              var deferred = $q.defer() ;
+              $http({method:'GET', url:'data/event/1.json'}).
                   success(function(data, status, headers, config)
                   {
-                     //  successcb(data) ;
+                     // successcb(data) ;
+                      deferred.resolve(data);
+
                   }).error(function(data, status, headers, config){
+                      deferred.reject(data) ;
                       $log.warn(data, status, headers, config);
                   })
+              return deferred.promise ;
           }
 
       }
